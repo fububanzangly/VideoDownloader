9dd921fc726ede9004b0d0fc8b4a04d7c7fa4a43
李扬 <ly18842607263@gmail.com>
2019-06-04 18:30:37 +0800

    fix bug

--- database.py
+++ database.py
@@ -3,11 +3,16 @@ import pickle
 try:
     f = open('db.db','rb')
     data = pickle.loads(f.read())
+    f.close()
 except :
+    f = open('db.db','wb')
     data=[[]for i in range(7)]
+    f.write(pickle.dumps(data))
+    f.close()
 
-dic={}
+
 def dump(name,zimuzu,day):
+    dic = {}
     f = open('db.db','wb')
     dic[name] = zimuzu
     if dic not in data[int(day)]:
@@ -16,7 +21,10 @@ def dump(name,zimuzu,day):
     f.close()
 
 def load(day):
+    f = open('db.db', 'rb')
     if day!=None:
+        f.close()
         return data[int(day)]
     else:
+        f.close()
         return data
